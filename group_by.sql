b6afaba27ee884aae70af8932c1639856ccabaa4
gianlutech <gianluca.ingrao95@gmail.com>
2022-03-09 18:37:57 +0100

    ex-1,2,3

---
+++ group_by.sql
@@ -0,0 +1,11 @@
+
+/* 1. Contare quanti iscritti ci sono stati ogni anno */
+SELECT COUNT(*) AS 'numero_studenti', `enrolment_date` FROM `students` GROUP BY `enrolment_date` ORDER BY `enrolment_date` ASC
+
+/* 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio */
+SELECT COUNT(*) AS 'insegnanti', `office_address` as 'stesso_ufficio' FROM `teachers` GROUP BY `office_address` ORDER BY `office_address`
+
+/* 3. Calcolare la media dei voti di ogni appello d'esame */
+SELECT AVG(vote) FROM `exam_student` WHERE `exam_id`
+
+/* 4. Contare quanti corsi di laurea ci sono per ogni dipartimento */
