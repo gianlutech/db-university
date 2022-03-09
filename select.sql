5c8327c36336d1de4f3c11e0c9ad77995711d361
gianlutech <gianluca.ingrao95@gmail.com>
2022-03-09 16:03:02 +0100

    ex-4

--- select.sql
+++ select.sql
@@ -7,3 +7,7 @@ SELECT * FROM `courses` WHERE `cfu` >= '10'
 
 /* Selezionare tutti gli studenti che hanno più di 30 anni */
 SELECT * FROM `students` WHERE `date_of_birth` <= '1992-01-01'
+
+/* Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
+laurea (286) */
+SELECT * FROM `courses` WHERE `period` = 'I semestre' AND `year` = '1'
