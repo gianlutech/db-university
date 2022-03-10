f493f28581eef86788ce0964c45a6271725c8a31
gianlutech <gianluca.ingrao95@gmail.com>
2022-03-10 18:21:50 +0100

    ex-6

--- relations.sql
+++ relations.sql
@@ -15,7 +15,7 @@ SELECT `students`.`surname` AS `cognome_studenti`, `students`.`name` AS `nome_st
 SELECT `degrees`.`name` AS `corsi_laurea` , `courses`.`name`AS `corsi`, `teachers`.`name` as `teachers` FROM `degrees` JOIN `courses` ON `degrees`.`id` = `courses`.`degree_id` JOIN `course_teacher` ON `degrees`.`id` = `course_teacher`.`teacher_id` JOIN `teachers` ON `teachers`.`id`= `course_teacher`.`course_id`
 
 /* 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54) */
-
+SELECT DISTINCT(`teachers`.`name`) as `name`, `teachers`.`surname` as `surname`, `departments`.`name` AS `departments` FROM `teachers` JOIN `course_teacher` ON `teachers`.`id` = `course_teacher`.`teacher_id` JOIN `courses` ON `courses`.`id` = `course_teacher`.`course_id` JOIN `degrees` ON `degrees`.`id` = `courses`.`degree_id` JOIN `departments` ON `departments`.id = `degrees`.`department_id` WHERE `departments`.`name` = 'Dipartimento di Matematica' ORDER BY `teachers`.`surname`
 
 /* 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
 superare ciascuno dei suoi esami */
