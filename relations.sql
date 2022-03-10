2d25110143a097932ea951ce2c529f5fd319b137
gianlutech <gianluca.ingrao95@gmail.com>
2022-03-10 17:11:06 +0100

    ex-3

--- relations.sql
+++ relations.sql
@@ -5,7 +5,7 @@ SELECT `students`.`name`as `name` , `students`.`surname`as `surname` , `degrees`
 SELECT `degrees`.`name` as `corsi`, `departments`.`name` as `dipartimento` FROM `degrees` JOIN `departments` ON `departments`.`id` = `degrees`.`department_id` WHERE `departments`.`name` = 'Dipartimento di Neuroscienze'
 
 /* 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44) */
-
+SELECT `courses`.`name` as `name` , `teachers`.`id` as `teacher`, `teachers`.`name` as `name`, `teachers`.`surname` as `surname` FROM `courses` JOIN `course_teacher` ON `courses`.`id` = `course_teacher`.`course_id` JOIN `teachers` ON `teachers`.`id` = `course_teacher`.`teacher_id` WHERE `teachers`.`id` = 44
 
 /* 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 relativo dipartimento, in ordine alfabetico per cognome e nome */
