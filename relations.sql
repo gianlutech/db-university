451ee5c5028b6bbcc0567151bcd9407a50a8ae1f
gianlutech <gianluca.ingrao95@gmail.com>
2022-03-10 17:40:14 +0100

    ex-4

--- relations.sql
+++ relations.sql
@@ -9,7 +9,7 @@ SELECT `courses`.`name` as `name` , `teachers`.`id` as `teacher`, `teachers`.`na
 
 /* 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 relativo dipartimento, in ordine alfabetico per cognome e nome */
-
+SELECT `students`.`surname` AS `cognome_studenti`, `students`.`name` AS `nome_studenti`, `degrees`.`name` as `corsi`, `departments`.`name` as `dipartimenti` FROM `students` JOIN `degrees` ON `degrees`.`id`= `students`.`degree_id` JOIN `departments` ON `departments`.`id`= `degrees`.`department_id` ORDER BY `students`.`surname`
 
 /* 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti */
 
